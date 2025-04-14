b92d43c0baa5bf0f4a2a44bfd7531912c7dc1d74
Andrews Innovations <david@andrewsinnovations.com>
2025-04-14 15:23:38 -0500

    Added posts to the single post renderer
    Added posts into the single post renderer so you can list out other posts within a post.

--- .post.sql
+++ .post.sql
@@ -34,6 +34,10 @@ set post = (
     select (sqlpage.run_sql('.post_data.sql')::jsonb) -> 0 -> 'posts' -> 0
 );
 
+set posts = (
+    select (sqlpage.run_sql('.post_data.sql')::jsonb) -> 0 -> 'posts'
+);
+
 set settings = (
     select (sqlpage.run_sql('.settings_data.sql')::jsonb) -> 0 -> 'settings'
 );
@@ -41,6 +45,7 @@ set settings = (
 select 
     'template_' || $template_id as component
     , JSON($post) as post
+    , JSON($posts) as posts
     , json($settings) as settings
 WHERE
     $post_id is not null;
